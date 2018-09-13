6079d6d5f54fa07e14d34a7a17a8c04461df5eb3
Rafael Alonso <rafaeljosealonsogerla@gmail.com>
2018-09-13 16:45:17 -0300

    cambios

---
+++ PR1AlonsoMiller/GameComm/Domain/Monster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Domain
+{
+    public class Monster : Character
+    {
+
+        public Monster(Player monsterPlayer)
+        {
+            player = monsterPlayer;
+            health = RoleValues.MONSTER_HEALTH;
+            attack = RoleValues.MONSTER_ATTACK;
+        }
+        public override void TakeDamage(int damage)
+        {
+            if (this.IsAlive())
+            {
+                health -= damage;
+            }
+        }
+    }
+}
